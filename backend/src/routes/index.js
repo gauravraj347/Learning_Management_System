9bd11ece41c682c8aa0ec517ec6af08bad50f660
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-04-19 02:00:38 +0530

    add course module

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes.js";
 import categoryRoutes from "../modules/category/category.routes.js";
+import courseRoutes from "../modules/course/course.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/courses", courseRoutes);
 
 export default router;
