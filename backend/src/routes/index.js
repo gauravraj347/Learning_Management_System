8eea309f774022fb01d2ce71ee2655cb2f6a148a
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-04-19 02:13:07 +0530

    add enrolment module

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes.js";
 import categoryRoutes from "../modules/category/category.routes.js";
 import courseRoutes from "../modules/course/course.routes.js";
+import enrollmentRoutes from "../modules/enrollment/enrollment.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/courses", courseRoutes);
-
+router.use("/enrollments", enrollmentRoutes);
 export default router;
